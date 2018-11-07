19384f5164fa35a3d2736f781dba88eb1b4785ab
Eirik Baug <eirikbaug@hotmail.com>
2018-11-07 15:51:10 +0100

    cleaned up a bit

--- i3/Scripts/lock.sh
+++ i3/Scripts/lock.sh
@@ -3,13 +3,13 @@
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
  
-if [[ -f $HOME/.config/i3/Icons/lock.png ]]
+if [[ -f $HOME/.config/Icons/lock.png ]]
 then
     # placement x/y
     PX=0
     PY=0
     # lockscreen image info
-    R=$(file ~/.config/i3/Icons/lock.png | grep -o '[0-9]* x [0-9]*')
+    R=$(file ~/.config/Icons/lock.png | grep -o '[0-9]* x [0-9]*')
     RX=$(echo $R | cut -d' ' -f 1)
     RY=$(echo $R | cut -d' ' -f 3)
  
@@ -24,7 +24,7 @@ then
         PX=$(($SROX + $SRX/2 - $RX/2))
         PY=$(($SROY + $SRY/2 - $RY/2))
  
-        convert /tmp/screen.png $HOME/.config/i3/Icons/lock.png -geometry +$PX+$PY -composite -matte  /tmp/screen.png
+        convert /tmp/screen.png $HOME/.config/Icons/lock.png -geometry +$PX+$PY -composite -matte  /tmp/screen.png
         echo "done"
     done
 fi
